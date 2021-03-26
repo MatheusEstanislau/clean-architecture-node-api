e8dfd067a8688e32db788a514c9e8d5fb98bdcf1
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-03-25 21:24:52 -0300

    test: ensure SignupController returns 500 if addAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -148,7 +148,7 @@ describe('Signup Controlle', () => {
     expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
   })
 
-  test('should return 400 if a invalid email provided', () => {
+  test('should return 500 if email validator throws', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error()
@@ -183,5 +183,23 @@ describe('Signup Controlle', () => {
       email: 'valid_email',
       password: 'any_password',
     })
+  })
+
+  test('should return 500 if addAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
